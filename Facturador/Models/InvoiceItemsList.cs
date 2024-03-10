30ee8a2eaf7d12f5b42a5637b66c8bb0406b5659
jdeleon <jdeleonferreira@gmail.com>
2024-03-10 11:11:59 +1100

    check TODOs 2

--- Facturador/Models/InvoiceItemsList.cs
+++ Facturador/Models/InvoiceItemsList.cs
@@ -5,9 +5,12 @@
         public int ItemRow { get; set; }
         public Item Item { get; set; }
         public float Quantity { get; set; }
+        //TODO: Check set to be private
         public decimal LineTotal { get; set; }
 
 
+        //TODO: Change to void.
+        //TODO: Change name to CalculateLineTotal
         public decimal ValueListItems(float Quantity  )
         {
             decimal LineTotal = (decimal) Quantity *  Item.UnitPrice;
